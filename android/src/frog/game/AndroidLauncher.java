26df2f2cc6be5d9e5071bd04594c96f9c2959316
antHogis <antonjhoglund@hotmail.com>
2018-03-06 13:03:36 +0200

    Initial commit

---
+++ android/src/frog/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package frog.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import frog.game.FrogMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FrogMain(), config);
+	}
+}
---
+++ desktop/src/frog/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package frog.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import frog.game.FrogMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FrogMain(), config);
+	}
+}
